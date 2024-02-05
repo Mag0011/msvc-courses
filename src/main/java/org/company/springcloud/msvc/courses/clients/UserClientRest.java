d692c68ff41c52bfaf965183edacbe9a98388e9d
Luis Enrique Magdaleno de la Cruz <luisenriquemagdalenodelacruz@192.168.1.19>
2024-02-05 12:36:38 -0600

    Springcloud added as dependency:
    * Code annotation for enabling discovery.
    * Added an argument into Dockerfile to enable TLSv 1.2 and avoid errors in K8s.
    * Speing cloud properties added.

--- src/main/java/org/company/springcloud/msvc/courses/clients/UserClientRest.java
+++ src/main/java/org/company/springcloud/msvc/courses/clients/UserClientRest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "msvc-users", url = "${msvc.user.url}")
+@FeignClient(value = "users-service")
 public interface UserClientRest {
 
     @GetMapping("/getById/{id}")
